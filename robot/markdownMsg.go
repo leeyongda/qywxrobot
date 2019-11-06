f7c073e1ecf47bbf0f2433528b3cbef72ce5daca
Coding <coding@yeeuu.com>
2019-11-06 16:18:34 +0800

    修复buf reset

--- robot/markdownMsg.go
+++ robot/markdownMsg.go
@@ -69,6 +69,7 @@ func (m *MarkdownCustomContent) WriteHref(title, href string) *MarkdownCustomCon
 }
 
 func (m *MarkdownCustomContent) WriteDone() *MarkDownMsgCustomize {
+  //defer m.buf.Reset()
 	return &MarkDownMsgCustomize{
 		Markdown: MarkdownContent{
 			Content: m.buf.String(),
